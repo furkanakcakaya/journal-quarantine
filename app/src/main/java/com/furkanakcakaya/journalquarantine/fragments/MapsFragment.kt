078e7afd6ea55b32a1d161a067122b2942f1885d
Furkan Akçakaya <furkanaxcakaya@gmail.com>
2022-06-10 18:27:29 +0300

    maps done

--- app/src/main/java/com/furkanakcakaya/journalquarantine/fragments/MapsFragment.kt
+++ app/src/main/java/com/furkanakcakaya/journalquarantine/fragments/MapsFragment.kt
@@ -18,16 +18,18 @@ class MapsFragment : Fragment() {
     private lateinit var binding:FragmentMapsBinding
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        
+        //observe livedata in viewmodel
+        viewModel.entries.observe(viewLifecycleOwner) {
+            it.forEach {
+                if ((it.latitude != null) && (it.longitude != null)) {
+                    googleMap.addMarker(
+                        com.google.android.gms.maps.model.MarkerOptions()
+                            .position(com.google.android.gms.maps.model.LatLng(it.latitude!!, it.longitude!!))
+                            .title(it.title)
+                    )
+                }
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/furkanakcakaya/journalquarantine/viewmodels/MapsViewModel.kt
+++ app/src/main/java/com/furkanakcakaya/journalquarantine/viewmodels/MapsViewModel.kt
@@ -1,12 +1,12 @@
 package com.furkanakcakaya.journalquarantine.viewmodels
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import com.furkanakcakaya.journalquarantine.repository.JournalRepository
 
-class MapsViewModel(application: Application) : ViewModel() {
+class MapsViewModel(application: Application) :  AndroidViewModel(application) {
     private val jRepo = JournalRepository(application)
-    private val entries = jRepo.getJournalData()
+    val entries = jRepo.getJournalData()
 
 
 }
